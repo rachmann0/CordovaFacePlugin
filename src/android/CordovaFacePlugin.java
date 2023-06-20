85b2fa6a46f2dc55575d41f4adbaaec13316c0f4
reece <reecedarby1889@gmail.com>
2023-06-20 11:30:26 +0700

    cannot find symbol

--- src/android/CordovaFacePlugin.java
+++ src/android/CordovaFacePlugin.java
@@ -2,6 +2,7 @@ package pluginid;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -56,12 +57,12 @@ public class CordovaFacePlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("checkPluginAvailable")) {
-            // PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-            // pluginResult.setKeepCallback(true);
-            // while (true){
-            //     callbackContext.success("true");
-            // }
-            return true;
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            while (true){
+                callbackContext.success("true");
+            }
+            // return true;
         }
         if (action.equals("initializeSDK")) {
             this.initializeSDK(callbackContext);
